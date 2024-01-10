326ed7c652a3d7907708cd5dfffca971ce8e14e4
Yussuf Seifudin <seifabdi4010@gmail.com>
2024-01-10 10:18:21 +0300

    DOM

--- file.js
+++ file.js
@@ -8,17 +8,15 @@ container.appendChild(content);
 
 // paragraph
 const paragraph = document.createElement('p');
-paragraph.classList.add('p');
 paragraph.textContent = 'Hey I am Red paragraph!';
-paragraph.setAttribute('style', 'color: red;'); 
-paragraph.appendChild(paragraph);
+paragraph.setAttribute('style', 'color: red;');
+document.body.appendChild(paragraph);
 
 // h3
 const h3 = document.createElement('p');
-h3.classList.add('h3');
 h3.textContent = 'Hey I am blue h3!';
-h3.setAttribute('style', 'color: blue;'); 
-h3.appendChild(h3);
+h3.setAttribute('style', 'color: blue;');
+document.body.appendChild(h3);
 
 // DIV
 const div = document.createElement('div');
